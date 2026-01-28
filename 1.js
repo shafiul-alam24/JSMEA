53134afff475ef2b61957ddbe891cfa88a0c9a03
shafiul-alam24 <azizbd08@gmail.com>
2026-01-28 14:13:47 +0600

    first commit

---
+++ 1.js
@@ -0,0 +1,7 @@
+let fruits = ["Apple", "Banana", "Mango"];
+fruits.push("Orange");               // Add to end
+console.log(fruits.join(", "));     // "Apple, Banana, Mango, Orange"
+
+let text = "Hello World";
+console.log(text.toUpperCase());    // "HELLO WORLD"
+console.log(text.slice(0, 5));      // "Hello"
